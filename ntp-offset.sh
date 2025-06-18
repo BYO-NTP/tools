ef1c3fd1afcf7c6497d48ab5576c24b6aac051ae
Matt Simerson <matt@tnpi.net>
2025-06-18 11:50:31 -0700

    offset: add shortcuts for ntp json driver

--- ntp-offset.sh
+++ ntp-offset.sh
@@ -76,8 +76,9 @@ which_log_file "$2"
 # finger friendly shortcuts
 PEER_FILTER="$1"
 case "$1" in
+    JSON|json) PEER_FILTER="JSON|127.127.46.0" ;;
     NMEA|nmea) PEER_FILTER="NMEA|127.127.20" ;;
-    PPS|pps)   PEER_FILTER="PPS|127.127.22" ;;
+    PPS|pps)   PEER_FILTER="PPS|127.127.22|127.127.46.128" ;;
 esac
 
 echo "Calculating offset for $PEER_FILTER in $LOGFILE"
@@ -87,6 +88,10 @@ echo "Calculating offset for $PEER_FILTER in $LOGFILE"
 #        ident     cnt     mean     rms      max     delay     dist     disp
 # ==========================================================================
 # 140.173.112.2     85   -0.509    1.345    4.606   80.417   49.260    1.092
+
+# chrony statistics.log
+# YYYY-MM-DD HH:MM:SS IP-Address    SRC-STD   SRC-Offset  Offset-STD Relative   Error     Ratio    Qty  Idx Run Asym
+# 2016-08-10 05:40:50 203.0.113.15  6.261e-03 -3.247e-03  2.220e-03  1.874e-06  1.080e-06 7.8e-02  16   0   8  0.00
 
 awk '
   # Sum the offsets for the specified peer
