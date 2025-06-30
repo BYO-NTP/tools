116e7a9a9d7940b19749a8f5eda5656de22f2c7b
Matt Simerson <matt@tnpi.net>
2025-06-29 20:35:05 -0700

    darwin,ntp, check for launchdaemon file too

--- ntp/disable.sh
+++ ntp/disable.sh
@@ -6,8 +6,11 @@ set -e
 is_running()
 {
 	case "$(uname -s)" in
-		FreeBSD|Darwin) pgrep -q "$1" ;;
-		Linux) pgrep -c "$1" > /dev/null 2>&1 ;;
+		FreeBSD) pgrep -q ntpd ;;
+        Darwin)
+            # pgrep ntpd can match for ntp and ntpsec
+            test -f /Library/LaunchDaemons/org.macports.ntp.plist && pgrep -q ntpd ;;
+		Linux) pgrep -c ntpd > /dev/null 2>&1 ;;
 		*)
 			echo "ERR: Unsupported platform $(uname -s). Please file a feature request."
 			exit 1
@@ -55,5 +58,5 @@ disable() {
     esac
 }
 
-if is_running ntpd; then stop; fi
+if is_running; then stop; fi
 if service_exists; then disable; fi
